69ffb34c6c8597d46b4eaffd5dd1315020b00e5e
lihinxgufin <34462408+lihinxgufin@users.noreply.github.com>
2020-04-16 10:37:19 +0700

    Add anotation

--- src/main/java/com/Library/mahasiswa/MahasiswaService.java
+++ src/main/java/com/Library/mahasiswa/MahasiswaService.java
@@ -1,11 +1,17 @@
 package com.Library.mahasiswa;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class MahasiswaService {
 	
 	// add list
 	// edited by: Muhamad Sholihin
+	@Autowired
 	private MahasiswaRepository repo;
 	
 	public List<Mahasiswa> listAll() {
